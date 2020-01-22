5d4d270e4ff25cad39455bc16dab48ffcdd9f7f3
GalbaKor <52763544+GalbaKor@users.noreply.github.com>
2020-01-22 18:51:58 +0000

    alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-document.getElementById("submit").onclick = function() {myAlert()}; 
+
+
+document.getElementById("submit").onclick = function() {myAlert()} 
 function myAlert() {
 	alert("Your message has been received and you will hear from us shortly!");
   }
